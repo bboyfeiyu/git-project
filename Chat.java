9ca87989420d0fcd105d9296456774f169d7d75b
Chaos <437700255@qq.com>
2015-08-15 11:43:15 +0800

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -1,5 +1,7 @@
 public class Chat {
     public static void main(String[] args) {
+    	 System.out.println("链接网络");
         System.out.println("Chat with me, I'm RD-A.");
+        System.out.println("关闭网络链接");
     }
-}
+}  
